dfbb86645c405ca8ee1d877b764f7523964a7bfb
raphlevy <raphlevy.perso@gmail.com>
2015-10-14 16:28:47 +0100

    Completed chapter 5

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
-var Post = require('./models/post');
+// var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function (req, res, next) {
-  Post.find(function(err, posts) {
-    if(err){return next(err)}
-    res.json(posts)
-  })
-});
 
-app.post('/api/posts', function(req, res, next){
-  var post = new Post({
-   username: req.body.username,
-   body: req.body.body
-  })
-  post.save(function (err, post) {
-    if (err) { return next (err)}
-      res.json(201, post)
-  })
-});
+// app.get('/api/posts', function (req, res, next) {
+//   Post.find()
+//   .sort('-date')
+//   .exec(function (err, posts) {
+//     if (err) {return next(err)}
+//     res.json(posts)
+//   })
+// });
 
+// app.post('/api/posts', function (req, res, next){
+//   var post = new Post({
+//    username: req.body.username,
+//    body: req.body.body
+//   })
+//   post.save(function (err, post) {
+//     if (err) { return next (err)}
+//       res.json(201, post)
+//   })
+// });
 
-app.listen(3000, function(){
+app.use('/api/posts', require('./controllers/api/posts'));
+
+app.use('/', require('./controllers/static'));
+
+// app.get('/', function (req, res) {
+//   res.sendfile('layouts/posts.html')
+// });
+
+app.listen(3000, function (){
   console.log('Server listining on', 3000)
 });
