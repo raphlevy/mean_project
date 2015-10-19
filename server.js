11bb4263744c89f7e56f43c46297413f3846ec0e
raphlevy <raphlevy.perso@gmail.com>
2015-10-19 17:45:18 +0100

    Starting chap 8 angular events

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use('/api/posts', require('./controllers/api/posts'));
 
 app.use('/', require('./controllers/static'));
 
+app.use('/api/sessions',
+require('./controllers/api/sessions'));
+
+app.use('/api/users',
+  require('./controllers/api/users'));
+
 app.listen(3000, function (){
   console.log('Server listining on', 3000)
 });
